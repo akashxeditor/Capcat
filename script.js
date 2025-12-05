f1577da2693ff673083a516f9cb87b1ebde39d8f
akashxeditor <vloggerboysofi786@gmail.com>
2025-12-05 23:34:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Tailwind custom config
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0f1114',
+        'card-bg': '#141618',
+        'secondary-card': '#1c1e22',
+        'primary-green': '#00c853',
+        'text-light': '#aaa',
+      }
+    }
+  }
+};
+
+<script
+  type="text/javascript"
+  src="//pl28196975.effectivegatecpm.com/cd/89/c0/cd89c0ce021ab290b6ab7e2335ab0c1f.js"
+></script>
+
